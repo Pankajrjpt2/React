c9298c1956633277f1606abb55918a1133b7624c
pankajrajput <pankajrjpt2@gmail.com>
2023-07-13 18:09:05 +0530

    1st code

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ]),
+]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
